9de3934fe18753319247f48e168003ebbb09946e
arnabb38 <arnabbasak896@gmail.com>
2020-10-01 23:56:48 +0600

    quick fix

--- pdf-reader.py
+++ pdf-reader.py
@@ -2,14 +2,14 @@ from gtts import gTTS
 import PyPDF2
 import os
 
-input_file = open(r'RH_StudyGuide_V2.pdf', 'rb')
+input_file = open('RH_StudyGuide_V2.pdf', 'rb')
 
 text = PyPDF2.PdfFileReader(input_file)
 
 pages = text.numPages
 print(pages)
 
-pageObj = text.getPage(0)
+pageObj = text.getPage(6)
 print(pageObj.extractText())
 
 language = 'en'
