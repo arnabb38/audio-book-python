f3d0110e29b2566ae2f57b7bb3522b0ec3033ea4
arnabb38 <arnabbasak896@gmail.com>
2020-10-01 23:44:08 +0600

    initial commit

---
+++ pdf-reader.py
@@ -0,0 +1,25 @@
+from gtts import gTTS
+import PyPDF2
+import os
+
+input_file = open(r'RH_StudyGuide_V2.pdf', 'rb')
+
+text = PyPDF2.PdfFileReader(input_file)
+
+pages = text.numPages
+print(pages)
+
+pageObj = text.getPage(0)
+print(pageObj.extractText())
+
+language = 'en'
+
+output = gTTS(text=text, lang=language, slow=False)
+
+# output.save("output.mp3")
+
+# os.system("rhythmbox output.mp3") #for Linux based system
+
+# os.system("./output.mp3") #for Linux based system
+
+# os.system("start output.mp3") #for Windows based system
