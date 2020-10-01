0672b48e5f1b082017ee5b736503a69b742b62dc
arnabb38 <arnabbasak896@gmail.com>
2020-10-02 00:43:47 +0600

    update mp3 conversion

--- pdf-reader.py
+++ pdf-reader.py
@@ -1,25 +1,18 @@
 from gtts import gTTS
 import PyPDF2
-import os
 
 input_file = open('RH_StudyGuide_V2.pdf', 'rb')
 
 text = PyPDF2.PdfFileReader(input_file)
 
-pages = text.numPages
-print(pages)
-
-pageObj = text.getPage(6)
-print(pageObj.extractText())
+print(text.numPages)
 
 language = 'en'
+output_text = ''
 
-output = gTTS(text=text, lang=language, slow=False)
+for pagenum in range (0, text.numPages):
+    pageObj = text.getPage(pagenum)
+    output_text = output_text + pageObj.extractText()
+    output = gTTS(text=output_text, lang=language, slow=False)
 
-# output.save("output.mp3")
-
-# os.system("rhythmbox output.mp3") #for Linux based system
-
-# os.system("./output.mp3") #for Linux based system
-
-# os.system("start output.mp3") #for Windows based system
+output.save("output.mp3")
